1a6074a9a8578d04cff7ada54258cfdc19023ac6
QWERTY1WASD <il06.03.77@gmail.com>
2023-07-17 10:59:39 +0500

    Add deck and rules change

---
+++ main.py
@@ -0,0 +1,27 @@
+from random import shuffle
+
+### DECK OF CARDS ###
+deck_of_cards = {
+    "Ace": 11,
+    "Jack": 2,
+    "Queen": 3,
+    "King": 4,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+}
+
+### RULES ###
+change_the_price_of_an_ace_when_going_through = True
+golden_ace = True
+five_identical_pictures = True
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
